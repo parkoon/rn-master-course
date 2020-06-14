9140879b64b0a1a2333719e5c3c7817d2c1cf9fe
parkoon <devparkoon@gmail.com>
2020-06-14 21:13:00 +0900

    feat: button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => {
+          console.log("Button Pressed");
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
