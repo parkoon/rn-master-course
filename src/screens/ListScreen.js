f4877b00074130d5c331bac865b7c5457203c232
parkoon <devparkoon@gmail.com>
2020-06-14 21:00:15 +0900

    feat: exercise

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,20 +3,20 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", age: 11 },
+    { name: "Friend #2", age: 12 },
+    { name: "Friend #3", age: 13 },
+    { name: "Friend #4", age: 14 },
+    { name: "Friend #5", age: 15 },
+    { name: "Friend #6", age: 16 },
+    { name: "Friend #7", age: 17 },
+    { name: "Friend #8", age: 18 },
+    { name: "Friend #9", age: 19 },
   ];
   return (
     <FlatList
       // 기본이 vertical
-      horizontal
+      horizontal={false}
       showsHorizontalScrollIndicator={false}
       keyExtractor={(el) => el.name}
       data={friends}
@@ -27,7 +27,13 @@ const ListScreen = () => {
         // 성능 이슈와 관계 있음!
         // 키를 따로 지정하지 않으면, 리액트는 모든 <Text />를 지우고 다시 재배열을 한다.
         // 키를 지정하면 그 키만 지운다.
-        return <Text style={styles.textStyle}>{el.item.name}</Text>;
+        return (
+          <View>
+            <Text style={styles.textStyle}>
+              {el.item.name} - {el.item.age}
+            </Text>
+          </View>
+        );
       }}
     />
   );
